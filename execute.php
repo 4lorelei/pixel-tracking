2b39a431817e0545cc0bef131d60d46a1921708a
4lorelei <b.sar7o@gmail.com>
2021-02-19 21:33:18 +0100

    no message

---
+++ execute.php
@@ -0,0 +1,13 @@
+<?php
+//$star_code = 0x1F31F;
+$star_code = 0x2605;
+setlocale(LC_TIME, 'it_IT');
+date_default_timezone_set('Europe/Rome');
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+if(!$update)
+{
+  exit;
+}
+
+print("Hello");
